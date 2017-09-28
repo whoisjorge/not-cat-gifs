70a003dfccee97607ac422a1db5b1f11c89abcf3
Jorge Moliner <email@whoisjorge.me>
2017-09-28 22:12:05 +0200

    oops  mixed content fix

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -18,7 +18,7 @@
 
       <br>
       <a href="http://js.org" target="_blank" title="JS.ORG | JavaScript Community">
-      <img src="http://logo.js.org/dark_horz.png" width="50" alt="JS.ORG Logo"/></a>
+      <img src="https://logo.js.org/dark_horz.png" width="50" alt="JS.ORG Logo"/></a>
   </section>
 </template>
 
